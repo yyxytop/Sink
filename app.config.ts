f207cf0e16dcf3ecd324230bbabedd9e281b019b
鬼鬼 <54203072+Akuma-real@users.noreply.github.com>
2024-09-14 08:17:14 +0000

    update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,6 @@
 export default defineAppConfig({
-  title: 'Sink',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  title: '短网址',
+  description: '一个简单、快速、安全的链接缩短工具，带有分析功能，100% 基于 Cloudflare 运行。',
   image: 'https://sink.cool/banner.png',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
